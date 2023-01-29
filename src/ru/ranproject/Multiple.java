9455d0a6dc4547296952c8bf292aaad603c6b3c7
SZhukov12 <zhukov-stv@mail.ru>
2023-01-30 00:06:32 +0500

    Initial commit

---
+++ src/ru/ranproject/Multiple.java
@@ -0,0 +1,24 @@
+package ru.ranproject;
+
+public class Multiple {
+    public static void main(String[] args)
+{   int a = 2 * 1;
+    int b = 2 * 2;
+    int c = 2 * 3;
+    int d = 2 * 4;
+    int e = 2 * 5;
+    int f = 2 * 6;
+    int g = 2 * 7;
+    int h = 2 * 8;
+    int i = 2 * 9;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+    System.out.println(h);
+    System.out.println(i);
+}
+}
